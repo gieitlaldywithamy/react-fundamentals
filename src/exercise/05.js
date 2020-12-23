4e7be3a0101bb4ac140aa3b0392e3c82c30bb269
Amy Morrison <amy.jeanette.morrison@gmail.com>
2020-12-23 01:50:13 +0000

    completed exercises

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,16 +12,27 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+
+const Box = ( { style, size='small', ...otherProps }) => (
+  <div
+    className={`box box--${size}`}
+    style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+  />
+)
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size='medium' style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size='large' style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,13 +1,78 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
+import * as React from 'react';
+
+function UsernameErrorForm({onSubmitUsername}) {
+  const [error, setError ] = React.useState(null);
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const isValid = value === value.toLowerCase();
+    setError( isValid ? null : 'Username must be lowercase' );
+  }
+
+  const handleSubmit=(event) => {
+    event.preventDefault();
+
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" type="text" onChange={handleChange} />
+      </div>
+      <div role="alert" style={{color: 'red'}}>
+        {error}
+      </div>
+      <button disabled={Boolean(error)} type="submit">
+        Submit
+      </button>
+    </form>
+  )
+}
+
+function ValidateUsername({onSubmitUsername}) {
+  const [username, setUsername] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmitUsername(username);
+  }
+
+  function handleChange(event) {
+    setUsername(event.target.value.toLowerCase());
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="usernameInput">Username:</label>
+        <input
+          id="usernameInput"
+          type="text"
+          onChange={handleChange}
+          value={username}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+
+}
 
 function UsernameForm({onSubmitUsername}) {
+  const inputEl = React.useRef(null);
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmitUsername(inputEl.current.value);
+  }
   //
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
@@ -19,10 +84,10 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input ref={inputEl} type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
@@ -31,7 +96,7 @@ function UsernameForm({onSubmitUsername}) {
 
 function App() {
   const onSubmitUsername = username => alert(`You entered: ${username}`)
-  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+  return <ValidateUsername onSubmitUsername={onSubmitUsername} />
 }
 
 export default App
